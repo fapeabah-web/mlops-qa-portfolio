c0f8590d660db5ce5efd3db3d279cdc0fb4a8732
Franklin Apea-Bah <fapeabah@gmail.com>
2026-09-20 03:44:48 -0500

    Add core control loop structures to data validation file

---
+++ 01_python_fundamentals/hello_qa.py
@@ -0,0 +1,42 @@
+print("Hello QA, Welcome!")
+print("Hello to the beautiful world of Python!")
+print(38.6 * 498.3)
+x = 38.6
+y = 498.3
+print(x * y)
+print(x / y)
+print(y / x)
+print(y % x)
+print(12 * x)
+z = 12 * x
+print(y - z)
+a = 12
+a = a * 46.3
+print(a)
+print(int(a))
+hours = 12
+rate = 20.95
+pay = hours * rate
+print(pay)
+print(int(pay))
+week1 = hours * 4
+week2 = hours * 3
+biweekly = week1 + week2
+print(biweekly)
+biweekly_pay = biweekly * rate
+print(biweekly_pay)
+income_tax = 0.108
+tax_amount = biweekly_pay * income_tax
+print(tax_amount)
+cpp_levy = 0.0595
+ei_levy = 0.0163
+union_dues = 0.02
+gp_insurance = 90
+deductions = (biweekly_pay * cpp_levy) + (biweekly_pay * ei_levy) + (biweekly_pay * union_dues) + gp_insurance + tax_amount
+net_pay = biweekly_pay - deductions
+print(net_pay)
+print(int(net_pay))
+x = 0.6
+x = 3.9 * x * (1- x)
+print(x)
+print(int(x))
