3a0000c5974cb7c43a727d9e5d279a4d327aafb1
Franklin Apea-Bah <fapeabah@gmail.com>
2026-09-23 21:59:27 -0500

    Refactor: Restructure workspace for clean Data Engineering and MLOps QA tracks

---
+++ 01_python_fundamentals/hello_world_idle_trial_Sep2026.py
@@ -0,0 +1,43 @@
+print('Hello, World!')
+x = 5
+if x < 10:
+    print('small')
+if x > 10:
+    print('large')
+print('finish')
+
+x = 5
+if x == 5:
+    print('equals 5')
+if x <= 5:
+    print('less than or equal to 5')
+if x >= 5:
+    print('greater than or equal to 5')
+if x > 5:
+    print('greater than 5')
+if x != 6:
+    print('not equal to 6')
+print("That's all")
+
+
+x = 5
+if x > 2:
+    print('bigger than 2')
+    print('still bigger')
+print('done with 2')
+
+for i in range(5):
+    print(i)
+    if i < 2:
+        print('smaller than 2')
+    if i >=2:
+        print('equal to or bigger than 2')
+    print('done with i', i)
+print('all done')
+
+x = 4
+if x > 2:
+    print('greater')
+else:
+    print('smaller')
+print('all done')
